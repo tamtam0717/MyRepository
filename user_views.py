57179109a2cf9376e1aa1a6da0208982b34a9b74
tamtam0717 <tamtam.atsushi.0717@gmail.com>
2024-02-25 15:54:20 +0900

    example.txtの追加と、viewsにedit_account_info関数を追加

--- user_views.py
+++ user_views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect  # たぶん、直前に開いていたページのままにするため
+  # 追加してみた1
 from user_management.models import Post, Like, Shop, Item, Cart, CartItem, PostCart, PostCartItem 
-from decimal import Decimal
+# 消してみた
 
 
 def account_view(request):
     request.session['url_tab_latest'] = 'account'
     return render(request, 'account.html')
+
+# 追加してみた２
+
+def edit_account_info_view(request):
+    return render(request, edit_account_info_view)
